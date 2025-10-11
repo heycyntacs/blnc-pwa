76298806015431cee577bb01f097333afa051363
heycyntacs <jiggybrillantes28@gmail.com>
2025-10-11 18:01:51 +0800

    feat: changed project name to blnc

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -4,7 +4,7 @@ import AuthButton from "./_components/auth-button";
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center gap-4 flex-col">
-      <h1 className="text-3xl font-bold">Pera</h1>
+      <h1 className="text-3xl font-bold">blnc</h1>
       <AuthButton provider={AUTH_PROVIDERS.GOOGLE} />
     </div>
   );

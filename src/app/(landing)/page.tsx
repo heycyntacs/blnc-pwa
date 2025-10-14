19b25623dab25c8cf0121326c131449fbda9f9b6
heycyntacs <jiggybrillantes28@gmail.com>
2025-10-14 23:08:35 +0800

    feat(landing): make header bigger

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center gap-4 flex-col">
-      <h1 className="text-3xl font-bold">blnc</h1>
+      <h1 className="text-7xl font-bold">blnc</h1>
     </div>
   );
 }

a9e965db0c4acff94d2bd36a6f041c2eedd32e5c
heycyntacs <jiggybrillantes28@gmail.com>
2025-10-15 00:16:59 +0800

    chore: add test comment

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center gap-4 flex-col">
+      {/* Header */}
       <h1 className="text-7xl font-bold">blnc</h1>
     </div>
   );
